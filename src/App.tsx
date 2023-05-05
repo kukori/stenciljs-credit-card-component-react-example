30b530bd737343c52c5cdacd3147bc616e54d010
kukori <attila.varjas@gmail.com>
2023-05-05 10:23:23 +0200

    Making the form work part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import './App.css'
 import { defineCustomElements, CreditCardInput, CardExpirationInput, CardFormContainer, CardHolderInput, CvvInput, ErrorMessage } from 'credit-card-components-react';
 import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
 
 defineCustomElements();
 
 function App() {
+
+  const creditCardSchema = Yup.object().shape({
+    creditCard: Yup.string().trim().min(18, 'Card number is not valid').required('Card number is not valid'),
+    cardExpiration: Yup.string().trim().min(5, 'Card expiration is not valid').required('Card expiration is not valid'),
+    cvv: Yup.string().trim().min(3, 'Cvv is not valid').required('Cvv is not valid'),
+    cardHolder: Yup.string().trim().min(1, 'Card holder is not valid').required('Card holder is not valid'),
+  });
+
+  const initialValues = {
+    creditCard: '',
+    cardExpiration: '',
+    cvv: '',
+    cardHolder: '',
+  };
+
   return (
     <div className="App">
       <Formik
-        initialValues={{}}
+        initialValues={initialValues}
         enableReinitialize={true}
-        // validationSchema={createCaseSchema}
+        validationSchema={creditCardSchema}
         onSubmit={(values) => console.log(values)}
       >
-        {({ errors, touched, dirty, values, setFieldValue, submitForm }) => (
+        {({ errors, touched, dirty, values, setFieldValue, submitForm}) => (
         <Form>
-        <CardFormContainer>
-          <CreditCardInput slot="credit-card" onChanged={(event) => console.log(event)}></CreditCardInput>
-          <CardExpirationInput slot="expiration"></CardExpirationInput>
-          <CvvInput slot="cvv"></CvvInput>
-          <CardHolderInput slot="card-holder"></CardHolderInput>
-        </CardFormContainer>
-        <div className='ErrorContainer'>
-          <ErrorMessage>The card number is required</ErrorMessage>
-        </div>
+          <CardFormContainer>
+            <CreditCardInput slot="credit-card" name="creditCard" value={values.creditCard} onBlur={(event) => console.log(event)} onChanged={(event) => setFieldValue('creditCard', event.target.value)} error={errors.creditCard ? true : false}></CreditCardInput>
+            <CardExpirationInput slot="expiration" name="cardExpiration" value={values.cardExpiration} onChanged={(event) => setFieldValue('cardExpiration', event.target.value)} error={errors.cardExpiration ? true : false}></CardExpirationInput>
+            <CvvInput slot="cvv" name="cvv" value={values.cvv} onChanged={(event) => setFieldValue('cvv', event.target.value)} error={errors.cvv ? true : false}></CvvInput>
+            <CardHolderInput slot="card-holder" name="cardHolder" value={values.cardHolder} onChanged={(event) => setFieldValue('cardHolder', event.target.value)} error={errors.cardHolder ? true : false}></CardHolderInput>
+          </CardFormContainer>
+          <div className='ErrorContainer'>
+            { errors.creditCard && <ErrorMessage>{errors.creditCard}</ErrorMessage>}
+            { errors.cardExpiration && <ErrorMessage>{errors.cardExpiration}</ErrorMessage>}
+            { errors.cvv && <ErrorMessage>{errors.cvv}</ErrorMessage>}
+            { errors.cardHolder && <ErrorMessage>{errors.cardHolder}</ErrorMessage>}
+          </div>
         </Form>)}
       </Formik>
     </div>
